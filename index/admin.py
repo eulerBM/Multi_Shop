2f03a806538da8998dca88926271768420b52d42
eulerBM <eullerborgesdamotta155@gmail.com>
2023-03-09 22:19:14 -0300

    Migraçãos Para os produtos

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from index.models import *
 
-# Register your models here.
+admin.site.register(review_person)
+admin.site.register(product)
--- index/models.py
+++ index/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+# Review
+class review_person(models.Model):
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=150)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+# Produto
+class product(models.Model):
+    choices_status = (
+        ('Eletrodoméstico', 'Eletrodoméstico'),
+        ('Beleza', 'Beleza'),
+        ('Acessórios', 'Acessórios'),
+        ('Roupas', 'Roupas'),
+
+    )
+    name = models.CharField(max_length=50, blank=False)
+    category = models.CharField(choices=choices_status, max_length=20 ,default='Categoria')
+    description = models.TextField(max_length=250, blank=False)
+    observation = models.TextField(max_length=150, blank=False)
+    price = models.FloatField(blank=False)
+    review = models.ManyToManyField(review_person)
+    image = models.ImageField(upload_to="image/", blank=False)
+
+    def __str__(self):
+        return self.name
+
