835048a7e4ea53ddf20cb392b9655b2424ef0848
eulerBM <eullerborgesdamotta155@gmail.com>
2023-03-25 21:30:23 -0300

    Detalhes do produto

--- detail/urls.py
+++ detail/urls.py
@@ -1,6 +1,7 @@
-from detail.views import detail
+from detail.views import detail, review
 from django.urls import path
 
 urlpatterns = [
     path('<int:id>', detail, name='detail' ),
+    path('review/<int:id>', review, name='review_user')
 ]
--- detail/views.py
+++ detail/views.py
@@ -1,17 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from index.models import product
+from index.models import product, review_person
+from django.views.decorators.http import require_GET
+from django.contrib.auth.decorators import login_required
+
 
 def detail(request, id):
 
-    get_product = product.objects.get(id=id)
-    paginador = Paginator(get_product, 1)
+    try:
+        # item product
+        get_product = product.objects.get(id=id)
 
-    context = {
+        # Review
+        get_review = review_person.objects.filter(product_review=id)
 
-        'products': get_product,
-        'paginador': paginador,
+    except:
+        return redirect ('index')
 
-    }
+    else:
+        user_paginator = Paginator(get_review, 1)
+        page = request.GET.get('page')
+        posts = user_paginator.get_page(page)
+        
 
-    return render (request, 'detail.html', context)
+        context = {
+
+            'products': get_product,
+            'posts': posts,
+
+        }
+
+        return render (request, 'detail.html', context)
+
+
+
+@login_required
+@require_GET
+def review(request, id):
+
+    try:
+        get_text = request.GET.get('text')
+        get_product = product.objects.get(id=id)
+
+    except:
+        return redirect ('index')
+    
+    else: 
+        review_person.objects.create(
+            person = request.user,
+            product_review = get_product,
+            text = get_text,
+        )
+
+        return redirect ('detail', id)
