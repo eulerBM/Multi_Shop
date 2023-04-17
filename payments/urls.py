084694030a71a89133884985450303caf903334d
eulerBM <eullerborgesdamotta155@gmail.com>
2023-04-16 22:11:15 -0300

    paginsa de sucesso e cancelado, pagamento

--- payments/urls.py
+++ payments/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from payments.views import *
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', HomePageView, name='home'),
     path('config/', stripe_config, name='config'),
     path('create-checkout-session/', create_checkout_session),  
+
+    #sucess or cancel
+    path('sucesso', sucess, name='sucess'),
+    path('cancelado', cancel, name='cancel'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,39 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings 
 from django.http.response import JsonResponse 
 from django.views.decorators.csrf import csrf_exempt 
 import stripe
 
-from django.views.generic.base import TemplateView
 
-class HomePageView(TemplateView):
-    template_name = 'pagamento/home_paga.html'
+def HomePageView(request):
+    return render (request, 'pagamento/home_paga.html')
 
 @csrf_exempt
 def stripe_config(request):
     if request.method == 'GET':
-        stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
+
+        stripe_config = {
+             
+            'publicKey': settings.STRIPE_PUBLISHABLE_KEY
+        }
+
         return JsonResponse(stripe_config, safe=False)
     
 @csrf_exempt
 def create_checkout_session(request):
-        domain_url = 'http://localhost:8000/pagar'
+        domain_url = 'http://localhost:8000/pagar/'
         stripe.api_key = settings.STRIPE_SECRET_KEY
-        try:
-            
+        try:    
             checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=domain_url + 'cancelled/',
                 payment_method_types=['card'],
-                mode='payment',
                 line_items=[
                     {   
                         'price':'price_1MsKXsE3RPdMzT3CGMuQAGeX',
                         'quantity': 1,
                         
                     }
-                ]
+                ],
+                mode='payment',
+                success_url=domain_url + 'sucesso',
+                cancel_url=domain_url + 'cancelado',
             )
             return JsonResponse({'sessionId': checkout_session['id']})
         except Exception as e:
             return JsonResponse({'error': str(e)})
+        
+def sucess(request):
+    return render (request, 'pagamento/sucess.html')
+
+def cancel(request):
+    return render (request, 'pagamento/cancel.html')
+
